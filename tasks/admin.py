1b419f0d222825b72c773519c68a5ef4ed150e16
zabavin <zabavin@gmail.com>
2021-12-20 14:31:43 +0300

    some models added admin's site

--- tasks/admin.py
+++ tasks/admin.py
@@ -1,3 +1,12 @@
 from django.contrib import admin
 
+from .models import Task, Project, Goal, Tag, Context, Contact
+
 # Register your models here.
+
+admin.site.register(Task)
+admin.site.register(Project)
+admin.site.register(Goal)
+admin.site.register(Tag)
+admin.site.register(Context)
+admin.site.register(Contact)
