e6c020f93540c348b551dee79cc6dfc3b1d634da
zabavin <zabavin@gmail.com>
2022-01-14 22:29:59 +0300

    custom task in admin's site

--- tasks/admin.py
+++ tasks/admin.py
@@ -4,7 +4,12 @@ from .models import Task, Project, Goal, Tag, Context, Contact
 
 # Register your models here.
 
-admin.site.register(Task)
+class TaskAdmin(admin.ModelAdmin):
+    list_display = ('folder', 'start_time', 'title')
+    list_filter = ('folder', 'goal', 'context', 'tags')
+    search_fields = ('title', 'note')
+
+admin.site.register(Task, TaskAdmin)
 admin.site.register(Project)
 admin.site.register(Goal)
 admin.site.register(Tag)
